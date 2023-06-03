369274156c565f4b33e860914df2b08bbd4dde5c
Nicolas Stirnemann <nicolas@brainhi.com>
2023-06-03 15:10:46 -0300

    Ziglings

---
+++ zig/ziglings/exercises/034_quiz4.zig
@@ -0,0 +1,27 @@
+//
+// Quiz time. See if you can make this program work!
+//
+// Solve this any way you like, just be sure the output is:
+//
+//     my_num=42
+//
+const std = @import("std");
+
+const NumError = error{IllegalNumber};
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    const my_num = getNumber(false);
+    if (my_num) |n| {
+        try stdout.print("my_num={}\n", .{n});
+    } else |err| {
+        try stdout.print("Err={}\n", .{err});
+    }
+}
+
+// This function is obviously weird and non-functional. But you will not be changing it for this quiz.
+fn getNumber(err: bool) NumError!u32 {
+    if (err) return NumError.IllegalNumber;
+    return 42;
+}
