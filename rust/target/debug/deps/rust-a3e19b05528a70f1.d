5429b898666ee2ef64bbde919e70b45ebd4bbe4d
Nicolas Stirnemann <nstirnemann59@gmail.com>
2023-08-07 19:13:38 -0300

    Remove rust - targer

--- rust/target/debug/deps/rust-a3e19b05528a70f1.d
+++
@@ -1,5 +0,0 @@
-/home/batmi/Projects/learn/rust/target/debug/deps/rust-a3e19b05528a70f1.rmeta: src/main.rs
-
-/home/batmi/Projects/learn/rust/target/debug/deps/rust-a3e19b05528a70f1.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/deps/rust-d43f9cb3bc5d87e6.d
+++
@@ -1,5 +0,0 @@
-/home/batmi/Projects/learn/rust/target/debug/deps/rust-d43f9cb3bc5d87e6.rmeta: src/main.rs
-
-/home/batmi/Projects/learn/rust/target/debug/deps/rust-d43f9cb3bc5d87e6.d: src/main.rs
-
-src/main.rs:
